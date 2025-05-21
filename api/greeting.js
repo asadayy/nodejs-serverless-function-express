c7d47d4f035b8cea32e696e83820562d3439f73a
asadayy <asadebukhari.work@gmail.com>
2025-05-21 16:14:14 +0500

    Update greeting.js

--- api/greeting.js
+++ api/greeting.js
@@ -1,1 +1,10 @@
-{"message":"I am Asad Bukhari!"}
+const obj = {
+  message: "I am Asad Bukhari!"
+};
+
+// To use it:
+console.log(obj.message); // Output: "I am Asad Bukhari!"
+
+// Or to convert it to a JSON string:
+const jsonString = JSON.stringify(obj);
+console.log(jsonString);
